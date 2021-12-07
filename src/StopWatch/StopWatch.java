6cd8dfbb01820ae9b4dfc250330f5a2a8e3d1266
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-12-07 15:33:33 +0700

    Add file

---
+++ src/StopWatch/StopWatch.java
@@ -0,0 +1,29 @@
+package StopWatch;
+
+public class StopWatch {
+    long startTime, endTime;
+
+    public StopWatch() {
+
+    }
+//    public StopWatch(double startTime, double endTime){
+//        this.startTime = startTime;
+//        this.endTime = endTime;
+//    }
+
+    public long getStartTime() {
+        startTime = System.currentTimeMillis();
+        return startTime;
+    }
+
+    public long getEndTime() {
+        endTime = System.currentTimeMillis();
+        return endTime;
+    }
+
+    public double stopWatch(){
+        return (endTime - startTime)/1000.0;
+    }
+
+}
+
